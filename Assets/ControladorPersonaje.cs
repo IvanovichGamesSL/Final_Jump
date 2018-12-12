f2dce404b3c9d796dcae2d4ab93365717893c583
DESKTOP-L6F0AGK\Arturo <atance.arturo@gmail.com>
2018-12-12 17:58:09 +0100

    2

--- Assets/ControladorPersonaje.cs
+++ Assets/ControladorPersonaje.cs
@@ -17,19 +17,33 @@ public class ControladorPersonaje : MonoBehaviour {
     void Start () {
         Time.timeScale = 0.5f;
         inicio = transform.position;
-	}
-	
-	// Update is called once per frame
+        puedoSaltar = false;
+    }
+
+    // Update is called once per frame
+    bool puedoSaltar = false;
 	void Update () {
         if (Input.GetKeyDown(KeyCode.Space)) {
             m_Rigidbody.AddForce(direccionEmpuje.forward * fuerzaEmpuje, ForceMode.Impulse);
-
         }
 
         if (Input.GetKeyDown(KeyCode.R)) {
             m_Rigidbody.transform.position = inicio;
             m_Rigidbody.transform.rotation = Quaternion.identity;
             
+        }
+
+        if (puedoSaltar && Input.GetAxis("Horizontal") < 0.9f) {
+            puedoSaltar = false;
+            m_Animator.SetTrigger("Saltar");
+            m_Rigidbody.AddForce(direccionEmpuje.forward * fuerzaEmpuje, ForceMode.Impulse);
+        }
+        if (Input.GetAxis("Horizontal") >= 0.9f) {
+            puedoSaltar = true;
+        }
+
+        if (!puedoSaltar && Input.GetAxis("Horizontal") > 0.9f) {
+
         }
         m_Animator.SetFloat("Agachado",Input.GetAxis("Horizontal"));
     }
